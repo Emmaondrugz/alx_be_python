16bcce51b0f1f050c93b6ecd63141014b916e542
Emmanuel Ekwere <ekwereemmanueludeme@gmail.com>
2025-11-09 09:33:17 +0000

    updated result from float to whole number

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -7,4 +7,5 @@ annual_interest = 0.05
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * annual_interest)
 
 print(f"Your monthly savings are ${monthly_savings}")
-print(f"Projected savings after one year, with interest, is: ${projected_savings}")
+print(f"Projected savings after one year, with interest, is: ${int(projected_savings)}")
+
