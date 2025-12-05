50e1b95d925760f14c6d3138ecd4c4437731ed65
Emmanuel Ekwere <ekwereemmanueludeme@gmail.com>
2025-12-05 01:13:45 +0000

    task 3 fix 3

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -12,14 +12,14 @@ class TestSimpleCalculator(unittest.TestCase):
     
     # 3. Write Test Methods for each operation
     
-    def test_add(self):
+    def test_addition(self):
         """Test the add method."""
         # 4. Use assertions to verify results
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(0, 0), 0)
     
-    def test_subtract(self):
+    def test_subtraction(self):
         """Test the subtract method."""
         self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(0, 5), -5)
