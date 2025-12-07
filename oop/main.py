a203f880d558bed72cb9d065643d2aad1dd54795
Emmanuel Ekwere <ekwereemmanueludeme@gmail.com>
2025-12-07 20:45:34 +0000

    week7 Task4 done

--- oop/main.py
+++ oop/main.py
@@ -1,5 +1,8 @@
 from book_class import Book
 from library_system import Book, EBook, PrintBook, Library
+from polymorphism_demo import Shape, Rectangle, Circle
+import math
+from class_static_methods_demo import Calculator
 
 def main():
 # Task 1
@@ -37,16 +40,27 @@ def main():
 
 
 # Task 3
-    def main():
-    shapes = [
-        Rectangle(10, 5),
-        Circle(7)
-    ]
+    #shapes = [
+    #    Rectangle(10, 5),
+    #    Circle(7)
+    #]
 
-    for shape in shapes:
-        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+    #for shape in shapes:
+    #    print(f"The area of the {shape.__class__.__name__} is: #{shape.area()}")
 
+
+
+# Task 4
+    	# Using the static method
+    	sum_result = Calculator.add(10, 5)
+    	print(f"The sum is: {sum_result}")
+
+    	# Using the class method
+    	product_result = Calculator.multiply(10, 5)
+    	print(f"The product is: {product_result}")
 
 
 if __name__ == "__main__":
     main()
+
+
