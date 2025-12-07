1e30e5143e5ee94d3eddf842cdbc95a5fd392e0f
Emmanuel Ekwere <ekwereemmanueludeme@gmail.com>
2025-12-07 20:48:41 +0000

    all fix done

--- oop/main.py
+++ oop/main.py
@@ -7,46 +7,46 @@ from class_static_methods_demo import Calculator
 def main():
 # Task 1
     # Creating an instance of Book
-    # my_book = Book("1984", "George Orwell", 1949)
+    my_book = Book("1984", "George Orwell", 1949)
 
     # Demonstrating the __str__ method
-    # print(my_book)  # Expected to use __str__
+    print(my_book)  # Expected to use __str__
 
     # Demonstrating the __repr__ method
-    # print(repr(my_book))  # Expected to use __repr__
+    print(repr(my_book))  # Expected to use __repr__
 
     # Deleting a book instance to trigger __del__
-    # del my_book
+    del my_book
 
 
 # Task 2
     # Create a Library instance
-    #my_library = Library()
+    my_library = Library()
 
     # Create instances of each type of book
-    #classic_book = Book("Pride and Prejudice", "Jane Austen")
-    #digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
-    #paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger",     234)
+    classic_book = Book("Pride and Prejudice", "Jane Austen")
+    digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
+    paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger",     234)
 
     # Add books to the library
-    #my_library.add_book(classic_book)
-    #my_library.add_book(digital_novel)
-    #my_library.add_book(paper_novel)
+    my_library.add_book(classic_book)
+    my_library.add_book(digital_novel)
+    my_library.add_book(paper_novel)
 
     # List all books in the library
-    #my_library.list_books()
+    my_library.list_books()
 
 
 
 
 # Task 3
-    #shapes = [
-    #    Rectangle(10, 5),
-    #    Circle(7)
-    #]
+    shapes = [
+        Rectangle(10, 5),
+        Circle(7)
+    ]
 
-    #for shape in shapes:
-    #    print(f"The area of the {shape.__class__.__name__} is: #{shape.area()}")
+    for shape in shapes:
+        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
 
 
 
